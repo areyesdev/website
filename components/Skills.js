66ea14a075197e69ddac1abe41976c683ceb7b25
Andres Reyes 👋 <ar97yf@gmail.com>
2021-07-01 12:09:25 -0500

    x:sparkles: [ADD] Skills Feature

---
+++ components/Skills.js
@@ -0,0 +1,113 @@
+/* eslint-disable simple-import-sort/imports */
+import { useTheme } from "next-themes";
+import Image from "next/image";
+
+const ExternalLink = ({ href, children }) => (
+  <a
+    className="text-sm text-gray-500 hover:text-gray-600 transition"
+    target="_blank"
+    rel="noopener noreferrer"
+    href={href}>
+    {children}
+  </a>
+);
+
+export default function Skills() {
+  const { theme } = useTheme();
+
+  const height = 90;
+  const width = 90;
+
+  return (
+    <>
+      <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-12 mt-14 text-black dark:text-white">
+        Skill tecnológicos y herramientas
+      </h3>
+
+      <div className="w-full grid grid-cols-3 md:grid-cols-4 grid-flow-row gap-y-8 gap-x-10 mb-16">
+        <ExternalLink href="https://developer.mozilla.org/es/docs/Web/HTML">
+          <Image alt="HTML logo" height={height} width={width} src="/logos/html.png" />
+        </ExternalLink>
+        <ExternalLink href="https://developer.mozilla.org/es/docs/Web/CSS">
+          <Image
+            alt="CSS logo"
+            height={height}
+            width={width}
+            src="/logos/css.png"
+            className="object-contain"
+          />
+        </ExternalLink>
+        <ExternalLink href="https://developer.mozilla.org/es/docs/Web/JavaScript">
+          <Image alt="JavaScript logo" height={height} width={width} src="/logos/js.png" />
+        </ExternalLink>
+        <ExternalLink href="https://git-scm.com/">
+          <Image alt="Git logo" height={height} width={width} src="/logos/git.png" />
+        </ExternalLink>
+        <ExternalLink href="https://reactjs.org/">
+          <Image
+            alt="React logo"
+            height={height}
+            width={width}
+            src="/logos/react.png"
+            className="object-cover"
+          />
+        </ExternalLink>
+        <ExternalLink href="https://nextjs.org/">
+          <Image
+            alt="Next.js logo"
+            height={height}
+            width={width}
+            src={theme === "light" ? "/logos/nextjs.png" : "/logos/nextjs_light.png"}
+            className="object-contain"
+          />
+        </ExternalLink>
+        <ExternalLink href="https://graphql.org/">
+          <Image alt="GraphQL logo" height={height} width={width} src="/logos/graphql.png" />
+        </ExternalLink>
+        <ExternalLink href="https://jestjs.io/">
+          <Image
+            alt="Jest logo"
+            height={height}
+            width={width}
+            src="/logos/jest.png"
+            className="object-contain"
+          />
+        </ExternalLink>
+        <ExternalLink href="https://webpack.js.org">
+          <Image
+            alt="Webpack logo"
+            height={height}
+            width={width}
+            src="/logos/webpack.png"
+            className="object-contain"
+          />
+        </ExternalLink>
+        <ExternalLink href="https://nodejs.org/">
+          <Image
+            alt="Node.js logo"
+            height={height}
+            width={width}
+            src="/logos/nodejs.png"
+            className="object-contain"
+          />
+        </ExternalLink>
+        <ExternalLink href="https://github.com/">
+          <Image
+            alt="GitHub logo"
+            height={height}
+            width={width}
+            src={theme === "light" ? "/logos/github_dark.png" : "/logos/github_light.png"}
+          />
+        </ExternalLink>
+        <ExternalLink href="https://code.visualstudio.com/">
+          <Image
+            alt="Visual Studio Code logo"
+            height={height}
+            width={width}
+            src="/logos/vscode.png"
+          />
+        </ExternalLink>
+      </div>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,31 @@
-import Image from 'next/image';
-import Container from '@/components/Container'
+import Image from "next/image";
+
+import Container from "@/components/Container";
+import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
-      <Container>
-       <div className="flex flex-col items-start justify-center max-w-2xl mx-auto mb-16">
+    <Container>
+      <div className="flex flex-col items-start justify-center max-w-2xl mx-auto mb-16">
         <h1 className="mb-4 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">
-        ¡Hola 👋! <br /> Soy Andres Reyes
+          ¡Hola 👋! <br /> Soy Andres Reyes
         </h1>
-          <div className="flex flex-col items-center py-4 mt-8 bg-gray-800 border-l-8 border-green-300 md:flex-row rounded-xl">
-            <h2 className="pl-4 text-lg text-gray-400 dark:text-gray-400 md:pr-4 mb-11 md:mb-0">
-            Soy Frontend Developer especializado en el ecosistema de JavaScript.
-            Amante de las buenas prácticas y del software de calidad. Soy
-            Frontend Developer especializado en el ecosistema de JavaScript.
-            Amante de las buenas prácticas y del software de calidad.
-            </h2>
-            <Image
+        <div className="flex flex-col items-center py-4 mt-8 bg-gray-800 border-l-8 border-green-300 md:flex-row rounded-xl">
+          <h2 className="pl-4 text-lg text-gray-400 dark:text-gray-400 md:pr-4 mb-11 md:mb-0">
+            Soy Frontend Developer especializado en el ecosistema de JavaScript. Amante de las
+            buenas prácticas y del software de calidad. Soy Frontend Developer especializado en el
+            ecosistema de JavaScript. Amante de las buenas prácticas y del software de calidad.
+          </h2>
+          <Image
             alt="Alexander Saavedra"
             height={900}
             width={900}
             src="/static/images/profile-new.jpg"
             className="object-cover rounded-full"
           />
-          </div>
         </div>
-      </Container>
-  )
+        <Skills />
+      </div>
+    </Container>
+  );
 }
