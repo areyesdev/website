a4974800a77dd97e63c98d11e13d068a2a827ebf
Andres Reyes <areyesdev@gmail.com>
2022-01-25 17:16:51 -0500

    feat: skill cards

--- components/Skills.js
+++ components/Skills.js
@@ -1,112 +1,116 @@
-/* eslint-disable simple-import-sort/imports */
-import { useTheme } from "next-themes";
 import Image from "next/image";
 
 const ExternalLink = ({ href, children }) => (
-  <a
-    className="text-sm text-gray-500 hover:text-gray-600 transition"
-    target="_blank"
-    rel="noopener noreferrer"
-    href={href}>
+  <a target="_blank" rel="noopener noreferrer" href={href}>
     {children}
   </a>
 );
 
 export default function Skills() {
-  const { theme } = useTheme();
-
   const height = 90;
   const width = 90;
 
   return (
     <>
-      <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-12  text-black dark:text-white">
+      <h3 className="font-bold text-2xl md:text-4xl tracking-tight text-black dark:text-white">
         Skill tecnológicos y herramientas
       </h3>
-
+      <p className="mb-6">
+        Me encanta aprender tecnologías nuevas y emocionantes. Estas son algunas de las que manejo
+        en mi 🌞.
+      </p>
+      {/* TODO: Improve the mobile version of skills */}
       <div className="w-full grid grid-cols-3 md:grid-cols-4 grid-flow-row gap-y-8 gap-x-10 mb-16">
-        <ExternalLink href="https://developer.mozilla.org/es/docs/Web/HTML">
-          <Image alt="HTML logo" height={height} width={width} src="/logos/html.png" />
-        </ExternalLink>
-        <ExternalLink href="https://developer.mozilla.org/es/docs/Web/CSS">
-          <Image
-            alt="CSS logo"
-            height={height}
-            width={width}
-            src="/logos/css.png"
-            className="object-contain"
-          />
-        </ExternalLink>
-        <ExternalLink href="https://developer.mozilla.org/es/docs/Web/JavaScript">
-          <Image alt="JavaScript logo" height={height} width={width} src="/logos/js.png" />
-        </ExternalLink>
-        <ExternalLink href="https://git-scm.com/">
-          <Image alt="Git logo" height={height} width={width} src="/logos/git.png" />
-        </ExternalLink>
-        <ExternalLink href="https://reactjs.org/">
-          <Image
-            alt="React logo"
-            height={height}
-            width={width}
-            src="/logos/react.png"
-            className="object-cover"
-          />
-        </ExternalLink>
-        <ExternalLink href="https://nextjs.org/">
-          <Image
-            alt="Next.js logo"
-            height={height}
-            width={width}
-            src={theme === "light" ? "/logos/nextjs.png" : "/logos/nextjs_light.png"}
-            className="object-contain"
-          />
-        </ExternalLink>
-        <ExternalLink href="https://graphql.org/">
-          <Image alt="GraphQL logo" height={height} width={width} src="/logos/graphql.png" />
-        </ExternalLink>
-        <ExternalLink href="https://jestjs.io/">
-          <Image
-            alt="Jest logo"
-            height={height}
-            width={width}
-            src="/logos/jest.png"
-            className="object-contain"
-          />
-        </ExternalLink>
-        <ExternalLink href="https://webpack.js.org">
-          <Image
-            alt="Webpack logo"
-            height={height}
-            width={width}
-            src="/logos/webpack.png"
-            className="object-contain"
-          />
-        </ExternalLink>
-        <ExternalLink href="https://nodejs.org/">
-          <Image
-            alt="Node.js logo"
-            height={height}
-            width={width}
-            src="/logos/nodejs.png"
-            className="object-contain"
-          />
-        </ExternalLink>
-        <ExternalLink href="https://github.com/">
-          <Image
-            alt="GitHub logo"
-            height={height}
-            width={width}
-            src={theme === "light" ? "/logos/github_dark.png" : "/logos/github_light.png"}
-          />
-        </ExternalLink>
-        <ExternalLink href="https://code.visualstudio.com/">
-          <Image
-            alt="Visual Studio Code logo"
-            height={height}
-            width={width}
-            src="/logos/vscode.png"
-          />
-        </ExternalLink>
+        <div className="bg-white rounded shadow-lg justify-center items-center flex flex-col dark:text-gray-700 moveUp">
+          <ExternalLink href="https://developer.mozilla.org/es/docs/Web/HTML">
+            <Image alt="HTML logo" height={height} width={width} src="/logos/html.png" />
+          </ExternalLink>
+        </div>
+        <div className="bg-white rounded shadow-lg justify-center items-center flex flex-col h-32 dark:text-gray-700 moveUp">
+          <ExternalLink href="https://developer.mozilla.org/es/docs/Web/CSS">
+            <Image
+              alt="CSS logo"
+              height={height}
+              width={width}
+              src="/logos/css.png"
+              className="object-contain"
+            />
+          </ExternalLink>
+        </div>
+        <div className="bg-white rounded shadow-lg justify-center items-center flex flex-col h-32 dark:text-gray-700 moveUp">
+          <ExternalLink href="https://developer.mozilla.org/es/docs/Web/JavaScript">
+            <Image alt="JavaScript logo" height={height} width={width} src="/logos/js.png" />
+          </ExternalLink>
+        </div>
+        <div className="bg-white rounded shadow-lg justify-center items-center flex flex-col h-32 dark:text-gray-700 moveUp">
+          <ExternalLink href="https://git-scm.com/">
+            <Image alt="Git logo" height={height} width={width} src="/logos/git.png" />
+          </ExternalLink>
+        </div>
+        <div className="bg-white rounded shadow-lg justify-center items-center flex flex-col h-32 dark:text-gray-700 moveUp">
+          <ExternalLink href="https://graphql.org/">
+            <Image alt="GraphQL logo" height={height} width={width} src="/logos/graphql.png" />
+          </ExternalLink>
+        </div>
+        <div className="bg-white rounded shadow-lg justify-center items-center flex flex-col h-32 dark:text-gray-700 moveUp">
+          <ExternalLink href="https://reactjs.org/">
+            <Image
+              alt="React logo"
+              height={height}
+              width={width}
+              src="/logos/react.png"
+              className="object-cover"
+            />
+          </ExternalLink>
+        </div>
+        <div className="bg-white rounded shadow-lg justify-center items-center flex flex-col h-32 dark:text-gray-700 moveUp">
+          <ExternalLink href="https://nextjs.org/">
+            <Image
+              alt="Next.js logo"
+              height={height}
+              width={width}
+              src="/logos/nextjs.png"
+              className="object-contain"
+            />
+          </ExternalLink>
+        </div>
+        <div className="bg-white rounded shadow-lg justify-center items-center flex flex-col h-32 dark:text-gray-700 moveUp">
+          <ExternalLink href="https://jestjs.io/">
+            <Image
+              alt="Jest logo"
+              height={height}
+              width={width}
+              src="/logos/jest.png"
+              className="object-contain"
+            />
+          </ExternalLink>
+        </div>
+        <div className="bg-white rounded shadow-lg justify-center items-center flex flex-col h-32 dark:text-gray-700 moveUp">
+          <ExternalLink href="https://nodejs.org/">
+            <Image
+              alt="Node.js logo"
+              height={height}
+              width={width}
+              src="/logos/nodejs.png"
+              className="object-contain"
+            />
+          </ExternalLink>
+        </div>
+        <div className="bg-white rounded shadow-lg justify-center items-center flex flex-col h-32 dark:text-gray-700 moveUp">
+          <ExternalLink href="https://github.com/">
+            <Image alt="GitHub logo" height={height} width={width} src="/logos/github_dark.png" />
+          </ExternalLink>
+        </div>
+        <div className="bg-white rounded shadow-lg justify-center items-center flex flex-col h-32 dark:text-gray-700 moveUp">
+          <ExternalLink href="https://code.visualstudio.com/">
+            <Image
+              alt="Visual Studio Code logo"
+              height={height}
+              width={width}
+              src="/logos/vscode.png"
+            />
+          </ExternalLink>
+        </div>
       </div>
     </>
   );
