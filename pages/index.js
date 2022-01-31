0049bdc5c18c2251eaf395d1cb24e006d8ab6105
Andres Reyes <areyesdev@gmail.com>
2022-01-31 08:28:22 -0500

    feat: add section contact

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 
 import Container from "@/components/Container";
 import Skills from "@/components/Skills";
+import Project from "@/components/Projects";
 
 export default function Home() {
   return (
@@ -33,109 +34,131 @@ export default function Home() {
           </div>
         </div>
         <Skills />
-        <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-8 mt-4 text-black dark:text-white">
-          🕵️Timeline
-        </h3>
-        <h3 className="text-lg md:text-xl font-bold mb-4 tracking-tight text-gray-900 dark:text-gray-100">
-          2021
-        </h3>
-        <ul>
-          <li className="mb-4 ml-2">
-            <div className="flex items-center mb-2 text-green-700 dark:text-green-300">
-              <span className="sr-only">Check</span>
-              <svg className="h-4 w-4 mr-2" viewBox="0 0 24 24">
-                <g
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round">
-                  <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
-                  <path d="M22 4L12 14.01l-3-3"></path>
-                </g>
-              </svg>
-              <p className=" text-lg text-gray-900 dark:text-gray-100">
-                Inicio mi nuevo trabajo 👌
-              </p>
-            </div>
-          </li>
-          <li className="mb-4 ml-2">
-            <div className="flex items-center mb-2 text-green-700 dark:text-green-300">
-              <span className="sr-only">Check</span>
-              <svg className="h-4 w-4 mr-2" viewBox="0 0 24 24">
-                <g
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round">
-                  <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
-                  <path d="M22 4L12 14.01l-3-3"></path>
-                </g>
-              </svg>
-              <p className=" text-lg text-gray-900 dark:text-gray-100">
-                Inicio mi nuevo trabajo 👌
-              </p>
-            </div>
-          </li>
-          <li className="mb-4 ml-2">
-            <div className="flex items-center mb-2 text-green-700 dark:text-green-300">
-              <span className="sr-only">Check</span>
-              <svg className="h-4 w-4 mr-2" viewBox="0 0 24 24">
-                <g
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round">
-                  <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
-                  <path d="M22 4L12 14.01l-3-3"></path>
-                </g>
-              </svg>
-              <p className=" text-lg text-gray-900 dark:text-gray-100">
-                Inicio mi nuevo trabajo 👌
-              </p>
-            </div>
-          </li>
-          <li className="mb-4 ml-2">
-            <div className="flex items-center mb-2 text-green-700 dark:text-green-300">
-              <span className="sr-only">Check</span>
-              <svg className="h-4 w-4 mr-2" viewBox="0 0 24 24">
-                <g
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round">
-                  <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
-                  <path d="M22 4L12 14.01l-3-3"></path>
-                </g>
-              </svg>
-              <p className=" text-lg text-gray-900 dark:text-gray-100">
-                Inicio mi nuevo trabajo dfasd SDFASDFG SDFASD dfgdfs 🙌
-              </p>
-            </div>
-          </li>
-          <li className="mb-4 ml-2">
-            <div className="flex items-center mb-2 text-green-700 dark:text-green-300">
-              <span className="sr-only">Check</span>
-              <svg className="h-4 w-4 mr-2" viewBox="0 0 24 24">
-                <g
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round">
-                  <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
-                  <path d="M22 4L12 14.01l-3-3"></path>
-                </g>
-              </svg>
-              <p className=" text-lg text-gray-900 dark:text-gray-100">
-                Inicio mi nuevo trabajo, nicio mi nuevo trabajo 👌
-              </p>
-            </div>
-          </li>
-        </ul>
+        <Project />
+        {/* <section>
+          <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-8 mt-4 text-black dark:text-white">
+            🕵️Timeline
+          </h3>
+          <h3 className="text-lg md:text-xl font-bold mb-4 tracking-tight text-gray-900 dark:text-gray-100">
+            2021
+          </h3>
+          <ul>
+            <li className="mb-4 ml-2">
+              <div className="flex items-center mb-2 text-green-700 dark:text-green-300">
+                <span className="sr-only">Check</span>
+                <svg className="h-4 w-4 mr-2" viewBox="0 0 24 24">
+                  <g
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round">
+                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                    <path d="M22 4L12 14.01l-3-3"></path>
+                  </g>
+                </svg>
+                <p className=" text-lg text-gray-900 dark:text-gray-100">
+                  Inicio mi nuevo trabajo 👌
+                </p>
+              </div>
+            </li>
+            <li className="mb-4 ml-2">
+              <div className="flex items-center mb-2 text-green-700 dark:text-green-300">
+                <span className="sr-only">Check</span>
+                <svg className="h-4 w-4 mr-2" viewBox="0 0 24 24">
+                  <g
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round">
+                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                    <path d="M22 4L12 14.01l-3-3"></path>
+                  </g>
+                </svg>
+                <p className=" text-lg text-gray-900 dark:text-gray-100">
+                  Inicio mi nuevo trabajo 👌
+                </p>
+              </div>
+            </li>
+            <li className="mb-4 ml-2">
+              <div className="flex items-center mb-2 text-green-700 dark:text-green-300">
+                <span className="sr-only">Check</span>
+                <svg className="h-4 w-4 mr-2" viewBox="0 0 24 24">
+                  <g
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round">
+                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                    <path d="M22 4L12 14.01l-3-3"></path>
+                  </g>
+                </svg>
+                <p className=" text-lg text-gray-900 dark:text-gray-100">
+                  Inicio mi nuevo trabajo 👌
+                </p>
+              </div>
+            </li>
+            <li className="mb-4 ml-2">
+              <div className="flex items-center mb-2 text-green-700 dark:text-green-300">
+                <span className="sr-only">Check</span>
+                <svg className="h-4 w-4 mr-2" viewBox="0 0 24 24">
+                  <g
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round">
+                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                    <path d="M22 4L12 14.01l-3-3"></path>
+                  </g>
+                </svg>
+                <p className=" text-lg text-gray-900 dark:text-gray-100">
+                  Inicio mi nuevo trabajo dfasd SDFASDFG SDFASD dfgdfs 🙌
+                </p>
+              </div>
+            </li>
+            <li className="mb-4 ml-2">
+              <div className="flex items-center mb-2 text-green-700 dark:text-green-300">
+                <span className="sr-only">Check</span>
+                <svg className="h-4 w-4 mr-2" viewBox="0 0 24 24">
+                  <g
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round">
+                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                    <path d="M22 4L12 14.01l-3-3"></path>
+                  </g>
+                </svg>
+                <p className=" text-lg text-gray-900 dark:text-gray-100">
+                  Inicio mi nuevo trabajo, nicio mi nuevo trabajo 👌
+                </p>
+              </div>
+            </li>
+          </ul>
+        </section> */}
+        <section>
+          <h2 className="font-bold text-2xl md:text-4xl tracking-tight text-black dark:text-white mb-4">
+            ¿Quieres Contactar?
+          </h2>
+          <p>
+            No dude en enviarme un mensaje si tiene alguna oferta interesante o alguna pregunta.
+          </p>
+          <p>
+            Utiliza{" "}
+            <a
+              href="mailto:areyesdev@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="no-underline">
+              areyesdev@gmail.com ✉️
+            </a>{" "}
+            para ponerse en contacto conmigo.
+          </p>
+        </section>
       </div>
     </Container>
   );
