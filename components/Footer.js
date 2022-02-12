440eae946e054c0065f6c4e5c3370ed06849f307
Andres Reyes <areyesdev@gmail.com>
2022-02-11 21:02:11 -0500

    feat(about): add description about me

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="h-16 w-full flex flex-col justify-center items-center">
+    <footer className=" h-16 w-full flex flex-col justify-center items-center">
       <div className="flex flex-row text-center text-gray-900 flex-wrap justify-center dark:text-gray-100">
         Made with&nbsp;<span className="text-blue-500">❤</span>&nbsp;@areyesdev
       </div>
--- components/MDXComponents.js
+++ components/MDXComponents.js
@@ -1,18 +1,37 @@
 /* eslint-disable jsx-a11y/heading-has-content */
 import Image from "next/image";
+import { GoMarkGithub } from "react-icons/go";
+
+import Skills from "./Skills";
 
 function RoundedImage(props) {
   return (
     <Image
       width={props.width}
       height={props.height}
-      alt={props.caption}
-      className="rounded-xl"
+      alt={props.alt}
+      className="rounded-xl "
       src={props.src}
     />
   );
 }
 
+function IconGithub(props) {
+  return (
+    <a
+      href={props.href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="text-gray-600 dark:text-white mr-2">
+      <GoMarkGithub size={props.size} />
+    </a>
+  );
+}
+
 export const MDXComponents = {
+  h2: (props) => <h1 className="font-bold text-5xl" {...props} />,
+  p: (props) => <p className="text-xl" {...props} />,
   Image: RoundedImage,
+  IconGithub,
+  Skills,
 };
